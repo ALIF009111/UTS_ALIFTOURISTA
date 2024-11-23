577f2e0f204b1a319cf94942cc8b288e14ee8509
ALIF009111 <aliftourista@gmail.com>
2024-11-23 11:55:32 +0700

    membuat proses php

---
+++ modul-manager/proses.php
@@ -0,0 +1,25 @@
+<?php
+include("../koneksi.php");
+
+
+
+$nama_karyawan = $_POST['nama_karyawan'];
+$jabatan = $_POST ['jabatan'];
+$tanggal_masuk = $_POST ['tanggal_masuk'];
+$gaji = $_POST ['gaji'];
+
+
+
+
+$simpan ="INSERT INTO karyawan (nama_karyawan,jabatan,tanggal_masuk,gaji) 
+VALUES ('$nama_karyawan','$jabatan','$tanggal_masuk','$gaji')";
+
+
+$proses = mysqli_query($koneksi,$simpan);
+
+
+
+?>
+<script>
+    document.location="index.php";
+</script>
